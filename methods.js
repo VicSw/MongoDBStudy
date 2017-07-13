cdc749e126cf07ab5fff9046eec8c5501bc9f930
sw <sw@sw.com>
2017-07-13 11:22:41 +0800

    update

--- methods.js
+++ methods.js
@@ -2,8 +2,49 @@ var mongoose=require('mongoose');
 
 mongoose.connect('mongodb://localhost/Mongo');
 
-var BookSchema=new mongoose.Schema({
+var UserSchema=new mongoose.Schema({
 	name:String,
 	isbn:Number
 });
+
+//声明了一个方法  自定义静态方法
+UserSchema.statics.findByISBN=function(isbn,cb){
+	this.findone({isbn:isbn},function(){
+		cb(err,doc);
+	});
+};
+
+//自定义实例方法
+UserSchema.methods.print=function(){
+	console.log('User Information:');
+	console.log('\tName:',this.name);
+
+}
+
+var User=mongoose.model('User',BooksSchema);
+
+var user=new User({
+	name:'sw'
+	isbn:123456
+});
+
+user.save(function(err){
+	if(err){
+		return console.log('save user faild ',err);
+	}
+
+	User.findByISBN(123456,function(err,doc){
+		console.log('findByISBN,err,doc:',err,doc);
+	});
+
+	Usre.print();
+
+
+});
+
+
+
+
+
+
 
